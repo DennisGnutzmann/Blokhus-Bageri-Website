88bf39c5f134bc89d52bc183269ee3a023eb8dba
Dennis Gnutzmann <stu204652@mail.uni-kiel.de>
2021-05-29 11:46:00 +0200

    Moved "database" to an external .js file

---
+++ js/database.js
@@ -0,0 +1,34 @@
+let rundstykke = [
+    {"name" : "testrundstyk",
+     "price" : 10,
+     "imageurl": ""
+    }
+]
+
+let broed = [
+    {"name" : "testbroed",
+     "price" : 20,
+     "imageurl": ""
+    }
+]
+
+let morgenkager = [
+    {"name" : "testkage",
+     "price" : 15,
+     "imageurl": ""
+    }
+]
+
+let eftermiddagskager = [
+    {"name" : "testkage",
+     "price" : 25,
+     "imageurl": ""
+    }
+]
+
+let diverse = [
+    {"name" : "sandwich",
+     "price" : 27,
+     "imageurl": ""
+    }
+]
--- js/shop.js
+++ js/shop.js
@@ -1,38 +1,3 @@
-let rundstykke = [
-    {"name" : "testrundstyk",
-     "price" : 10,
-     "imageurl": ""
-    }
-]
-
-let broed = [
-    {"name" : "testbroed",
-     "price" : 20,
-     "imageurl": ""
-    }
-]
-
-let morgenkager = [
-    {"name" : "testkage",
-     "price" : 15,
-     "imageurl": ""
-    }
-]
-
-let eftermiddagskager = [
-    {"name" : "testkage",
-     "price" : 25,
-     "imageurl": ""
-    }
-]
-
-let diverse = [
-    {"name" : "sandwich",
-     "price" : 27,
-     "imageurl": ""
-    }
-]
-
 function getPriceByName(name,array) {
     return array.find(element => element.name = name).price;
 }
