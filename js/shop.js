20404bdbedff90ef34f07bd03c7c198622ea037b
Dennis Gnutzmann <stu204652@mail.uni-kiel.de>
2021-06-02 15:40:53 +0200

    Added simple cookie for testing purposes

--- js/shop.js
+++ js/shop.js
@@ -110,6 +110,7 @@ function refreshCart() {
     totalprice.innerHTML = 'Total: ' + getTotalPrice() + ' DKK';
     //Recalculate total amount of items
     document.getElementById("cart-name").innerHTML= '('+getTotalItems()+')';
+    setCookie();
 }
 
 function testCheckOut() {
@@ -117,3 +118,45 @@ function testCheckOut() {
     refreshCart();
     alert("Thank you for your purchase!");
 }
+
+function init() {
+    generateTables();
+    checkCookie();
+    //getCookie();
+    //refreshCart();
+}
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+  
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+  
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
+ }
