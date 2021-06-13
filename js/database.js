2e4febf121f0f785991097c302ce8ed1092c62ea
Dennis Gnutzmann <stu204652@mail.uni-kiel.de>
2021-06-13 16:12:02 +0200

    Initial fill of "database"

--- js/database.js
+++ js/database.js
@@ -1,39 +1,360 @@
 let rundstykke = [
-    {"name" : "testrundstyk",
-     "price" : 10,
-     "imageurl": "",
-     "orderable": true
-    }
+{"name" : "Spansk",
+"price" : 6.50,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Alm. med sesam",
+"price" : 6.50,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Håndværker",
+"price" : 6.50,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Skagenslap",
+"price" : 6.50,
+"imageurl": "",
+"orderable": true
+ },
+ {"name" : "Søstjerne",
+ "price" : 6.50,
+ "imageurl": "",
+ "orderable": true
+},
+{"name" : "Baguette",
+"price" : 8.50,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Italiensk",
+"price" : 8.50,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Grøv italiensk",
+"price" : 8.50,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Birkes",
+"price" : 8.50,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Grøv birkes",
+"price" : 8.50,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Gulerødsboller",
+"price" : 9.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Tilbud: 5 Gulerødsboller",
+"price" : 33.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Krydde",
+"price" : 6.50,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Teboller",
+"price" : 7.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Chokoladeboller",
+"price" : 9.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Rosinboller",
+"price" : 9.00,
+"imageurl": "",
+"orderable": true
+}
 ]
 
 let broed = [
-    {"name" : "testbroed",
-     "price" : 20,
-     "imageurl": "",
-     "orderable": true
-    }
+{"name" : "Softkerne rugbrød (80%)",
+"price" : 30.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Schackenborg rugbrød (60%)",
+"price" : 30.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Lille alm. franskbrød",
+"price" : 18.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Lille franskbrød med hvid birkes",
+"price" : 18.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Alm. franskbrød",
+"price" : 27.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Franskbrød med hvid birkes",
+"price" : 27.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Håndværkerbrød",
+"price" : 27.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Valnøddebrød",
+"price" : 29.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Majsbrød",
+"price" : 29.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Græskarkernebrød",
+"price" : 27.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Blokhus Grovbbrød",
+"price" : 29.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Italiensk brød",
+"price" : 30.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Grøv italiensk brød",
+"price" : 30.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Flute",
+"price" : 13.00,
+"imageurl": "",
+"orderable": true
+}
 ]
 
 let morgenkager = [
-    {"name" : "testkage",
-     "price" : 15,
-     "imageurl": "",
-     "orderable": true
-    }
+{"name" : "Croissant",
+"price" : 12.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Københavnerbirkes",
+"price" : 12.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Spandauer med creme",
+"price" : 14.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Spandauer med syltetøj",
+"price" : 14.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Kanelsnegl",
+"price" : 14.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Hanekam",
+"price" : 14.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Brunsviger",
+"price" : 14.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Kærnemælkshorn",
+"price" : 14.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Dagmartærte",
+"price" : 40.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Kanelstang",
+"price" : 46.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Æblestang",
+"price" : 46.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Chokoladestang",
+"price" : 46.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Brunsvigerstang",
+"price" : 46.00,
+"imageurl": "",
+"orderable": true
+},
+{"name" : "Lille Wienerbrødkagemand (til 8 personer)",
+"price" : 145.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Wienerbrødkagemand (til 15 personer)",
+"price" : 185.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Lille Brunsvigerkagemand (til 8 personer)",
+"price" : 145.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Brunsvigerkagemand (til 15 personer)",
+"price" : 185.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Hindbærkagemand (til 15 personer)",
+"price" : 200.00,
+"imageurl": "",
+"orderable": false
+}
 ]
 
 let eftermiddagskager = [
-    {"name" : "testkage",
-     "price" : 25,
-     "imageurl": "",
-     "orderable": false
-    }
+{"name" : "Romkugle",
+"price" : 9.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Tilbud: 5 Romkugler",
+"price" : 30.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Træstamme",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Napoleonshat",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Brownie",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Valnøddeguf",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Nøddehorn",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Hindbærsnitte",
+"price" : 15.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Nürenbergsnitte",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Nougatsnitte",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Mazarin-/Toscasnitte",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Makronsnitte",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Æblesnitte",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Sveskesnitte",
+"price" : 17.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Kajkage",
+"price" : 19.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Mozartkage",
+"price" : 19.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Lille jordbærtærte",
+"price" : 21.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Jordbærtærte",
+"price" : 75.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Jordbærstang",
+"price" : 80.00,
+"imageurl": "",
+"orderable": false
+},
+
 ]
 
 let diverse = [
-    {"name" : "sandwich",
-     "price" : 27,
-     "imageurl": "",
-     "orderable": false
-    }
+{"name" : "Kylling-bacon sandwich",
+"price" : 35.00,
+"imageurl": "",
+"orderable": false
+},
+{"name" : "Pølsehorn",
+"price" : 18.00,
+"imageurl": "",
+"orderable": false
+}
 ]
