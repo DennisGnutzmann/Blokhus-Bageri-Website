641e2d96be63a0200c8744e22f6f86d1f06cddeb
Dennis Gnutzmann <stu204652@mail.uni-kiel.de>
2021-06-18 12:42:08 +0200

    Fixed some language inconsistencies with cart

--- js/shop.js
+++ js/shop.js
@@ -31,7 +31,21 @@ function generateTable(array,name) {
            button.setAttribute("onclick","addProduct('"+array[i-1].name+"',"+array[i-1].price+",1)");
            buttoncell.appendChild(button);
         } else {
-           buttoncell.innerHTML = "Cannot be ordered."
+            //Put correct text based on htmls lang attribute
+           let lang = document.getElementsByTagName('html')[0].getAttribute('lang');
+           switch(lang) {
+            case "da":
+            buttoncell.innerHTML = "Kan ikke bestilles."
+            break;
+            case "de":
+            buttoncell.innerHTML = "Nicht bestellbar."
+            break;
+            case "en":
+            buttoncell.innerHTML = "Cannot be ordered."
+            break;
+            default:
+            buttoncell.innerHTML = "Cannot be ordered."
+          }
         }
     }
 }
@@ -72,8 +86,10 @@ function getTotalItems() {
 function addProduct(name,price,amount) {
     let product = new Product(name,price,amount);
     let entry = cart.find(element => element.name == name);
+    //Add to cart if product type is not yet in the cart
     if (entry==null) {
         cart.push(product);
+    //Otherwise add to existing product amount    
     } else {
         entry.amount = entry.amount + amount;
         entry.totalprice = entry.totalprice + amount*price;
@@ -91,23 +107,23 @@ function removeProduct(name) {
 }
 
 function refreshCart() {
-    let table = document.getElementById("carttable");
-    table.innerHTML = "<tr><th>Name</th><th>Price</th><th>Amount</th></tr>";
-    for (var i=1; i<=cart.length; i++) {
+    let table = document.getElementById("cart-tbody");
+    table.innerHTML = "";
+    for (var i=0; i<cart.length; i++) {
         //Create new tr
         let row = table.insertRow(i);
 
         //Add and fill td for name
         let namecell = row.insertCell(0);
-        namecell.innerHTML = cart[i-1].name;
+        namecell.innerHTML = cart[i].name;
 
         //Add and fill td for price
         let pricecell = row.insertCell(1);
-        pricecell.innerHTML = cart[i-1].price.toFixed(2) + ' DKK';
+        pricecell.innerHTML = cart[i].price.toFixed(2) + ' DKK';
 
         //Add and fill td for price
         let amountcell = row.insertCell(2);
-        amountcell.innerHTML = cart[i-1].amount;
+        amountcell.innerHTML = cart[i].amount;
         let button = document.createElement("BUTTON");
         button.type = "button";
         button.innerHTML = "X";
@@ -116,14 +132,41 @@ function refreshCart() {
     }
     //Recalculate total price
     let totalprice = document.getElementById("total-price");
-    totalprice.innerHTML = 'Total: ' + getTotalPrice().toFixed(2) + ' DKK';
+    let lang = document.getElementsByTagName('html')[0].getAttribute('lang');
+    switch(lang) {
+     case "da":
+     totalprice.innerHTML = 'Total: ' + getTotalPrice().toFixed(2) + ' DKK'; 
+     break;
+     case "de":
+     totalprice.innerHTML = 'Gesamt: ' + getTotalPrice().toFixed(2) + ' DKK';
+     break;
+     case "en":
+     totalprice.innerHTML = 'Total: ' + getTotalPrice().toFixed(2) + ' DKK';
+     break;
+     default:
+     totalprice.innerHTML = 'Total: ' + getTotalPrice().toFixed(2) + ' DKK';
+   }
+
     //Recalculate total amount of items
     refreshCartAmount();
     setCookie();
 }
 
 function refreshCartAmount() {
-  document.getElementById("cart-name").innerHTML= 'Your Cart ('+getTotalItems()+')'; 
+    let lang = document.getElementsByTagName('html')[0].getAttribute('lang');
+    switch(lang) {
+     case "da":
+     document.getElementById("cart-name").innerHTML= 'Din kurv ('+getTotalItems()+')'; 
+     break;
+     case "de":
+     document.getElementById("cart-name").innerHTML= 'Dein Korb ('+getTotalItems()+')'; 
+     break;
+     case "en":
+     document.getElementById("cart-name").innerHTML= 'Your Cart ('+getTotalItems()+')'; 
+     break;
+     default:
+     document.getElementById("cart-name").innerHTML= 'Your Cart ('+getTotalItems()+')'; 
+   }
 }
 
 function testCheckOut() {
